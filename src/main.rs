e4664851ee3c29e8a1e639989d1d4b8d3e387b22
kuroneko913 <myblackcat7112+github@gmail.com>
2025-01-01 21:58:02 +0900

    [fix] 波括弧でブロックをブロックとして処理する

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,123 @@
-fn main() {
-    for line in std::io::stdin().lines() {
-        let mut stack = vec![]; // mutableなスタックを用意
+#[derive(Debug, PartialEq, Eq)]
+enum Value<'src> {
+  Num(i32),
+  Op(&'src str),
+  Block(Vec<Value<'src>>),
+}
 
-        if let Ok(line) = line {
-            // let words: Vec<_> = line.split(" ").collect(); // splitはイテレータを返すのでcollectでVecに変換
-
-            // パースした文字列が数値ならスタックに積む
-            for word in line.split(" ") {
-                if let Ok(parsed) = word.parse::<i32>() {
-                    stack.push(parsed);
-                } else {
-                    match word {
-                        // 演算子が来たらスタックから取り出して計算
-                        "+" => add(&mut stack),
-                        "-" => sub(&mut stack),
-                        "*" => mul(&mut stack),
-                        "/" => div(&mut stack),
-                        _ => panic!("unknown operator: {word:?}"),
-                    }
-                }
-            }   
-        }
-        println!("stack: {stack:?}");
+impl<'src> Value<'src> {
+    fn as_num(&self) -> i32 {
+      match self {
+        Self::Num(val) => *val,
+        _ => panic!("Value is not a number"),
+      }
     }
 }
 
-fn add(stack: &mut Vec<i32>) {
-    let a = stack.pop().unwrap();
-    let b = stack.pop().unwrap();
-    stack.push(a + b);
+fn main() {
+    for line in std::io::stdin().lines().flatten() {
+        parse(&line);
+    }
 }
-fn sub(stack: &mut Vec<i32>) {
-    let a = stack.pop().unwrap();
-    let b = stack.pop().unwrap();
-    stack.push(a - b);
+
+fn parse<'a>(line: &'a str)-> Vec<Value<'a>> {
+    let mut stack = vec![];
+    let input: Vec<_> = line.split(" ").collect();
+    let mut words = &input[..]; // 全範囲を意味する
+
+    // 配列が空でない場合、最初の要素（&T）と残りの要素（&[T]）を含むタプルを返す
+    while let Some((&word, mut rest)) = words.split_first() {
+       if word.is_empty() {
+           break;
+       }
+       if word == "{" {
+           let value;
+           // 今見ている要素が { だった場合、parse_block を呼び出して、それ以降の部分をパースする
+           (value, rest) = parse_block(rest);
+           stack.push(value);
+       } else if let Ok(parsed) = word.parse::<i32>() {
+            // 数字の場合は、Num としてスタックに積む
+           stack.push(Value::Num(parsed));
+       } else {
+            // 数字、{} 以外の場合、演算子として処理する
+            match word {
+                "+" => add(&mut stack),
+                "-" => sub(&mut stack),
+                "*" => mul(&mut stack),
+                "/" => div(&mut stack),
+                _ => panic!("{word:?} could not be parsed"),
+            }
+       }
+       words = rest;
+    }
+
+    println!("stack: {stack:?}");
+
+    stack
 }
-fn mul(stack: &mut Vec<i32>) {
-    let a = stack.pop().unwrap();
-    let b = stack.pop().unwrap();
-    stack.push(a * b);
+
+fn parse_block<'src, 'a>(
+    input: &'a [&'src str],
+) -> (Value<'src>, &'a [&'src str]) {
+    let mut tokens = vec![];
+    let mut words = input;
+
+    while let Some((&word, mut rest)) = words.split_first() {
+        if word.is_empty() {
+            break;
+        }
+        // {}の中身をtokensに保持して、}が来たら値(数値と演算子を含むオブジェクト)を返却する
+        if word == "{" {
+            let value;
+            (value, rest) = parse_block(rest);
+            tokens.push(value);
+        } else if word == "}" {
+            // {}の中身はBlockとして保持する
+            return (Value::Block(tokens), rest);
+        } else if let Ok(value) = word.parse::<i32>() {
+            tokens.push(Value::Num(value)); // 数字は数字で保持する
+        } else {
+            tokens.push(Value::Op(word)); // それ以外は演算子として保持する
+        }
+        words = rest;
+    }
+    
+    // 最後にtokensを返却する
+    (Value::Block(tokens), words)
 }
-fn div(stack: &mut Vec<i32>) {
-    let a = stack.pop().unwrap();
-    let b = stack.pop().unwrap();
-    stack.push(a / b);
+
+fn add(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs + rhs));
+}
+  
+fn sub(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs - rhs));
+}
+  
+fn mul(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs * rhs));
+}
+  
+fn div(stack: &mut Vec<Value>) {
+    let rhs = stack.pop().unwrap().as_num();
+    let lhs = stack.pop().unwrap().as_num();
+    stack.push(Value::Num(lhs / rhs));
+}
+
+#[cfg(test)]
+mod test {
+  use super::{parse, Value::*};
+  #[test]
+  fn test_group() {
+    assert_eq!(
+      parse("1 2 + { 3 4 }"),
+      vec![Num(3), Block(vec![Num(3), Num(4)])]
+    );
+  }
 }
