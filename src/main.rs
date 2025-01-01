1ac00a6f6a1636ca005c476c28472d24dd39717c
kuroneko913 <myblackcat7112+github@gmail.com>
2025-01-01 19:49:27 +0900

    [fix] 標準入力から受け取った逆ポーランド記法での計算式を四則演算式として評価する

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
 fn main() {
-    let mut stack = vec![];
+    for line in std::io::stdin().lines() {
+        let mut stack = vec![]; // mutableなスタックを用意
 
-    stack.push(42);
-    stack.push(36);
-    
-    add(&mut stack);
+        if let Ok(line) = line {
+            // let words: Vec<_> = line.split(" ").collect(); // splitはイテレータを返すのでcollectでVecに変換
 
-    stack.push(22);
-
-    add(&mut stack);
-
-    println!("stack: {stack:?}");
+            // パースした文字列が数値ならスタックに積む
+            for word in line.split(" ") {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        // 演算子が来たらスタックから取り出して計算
+                        "+" => add(&mut stack),
+                        "-" => sub(&mut stack),
+                        "*" => mul(&mut stack),
+                        "/" => div(&mut stack),
+                        _ => panic!("unknown operator: {word:?}"),
+                    }
+                }
+            }   
+        }
+        println!("stack: {stack:?}");
+    }
 }
 
 fn add(stack: &mut Vec<i32>) {
-    let lhs = stack.pop().unwrap();
-    let rhs = stack.pop().unwrap();
-    stack.push(lhs + rhs);
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    stack.push(a + b);
+}
+fn sub(stack: &mut Vec<i32>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    stack.push(a - b);
+}
+fn mul(stack: &mut Vec<i32>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    stack.push(a * b);
+}
+fn div(stack: &mut Vec<i32>) {
+    let a = stack.pop().unwrap();
+    let b = stack.pop().unwrap();
+    stack.push(a / b);
 }
