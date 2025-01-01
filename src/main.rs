eaf912a4dc6d0bb485a0c99e26809524d824929f
kuroneko913 <myblackcat7112+github@gmail.com>
2025-01-01 19:14:15 +0900

    [add] スタックの中身を2つ取り出して加算したものをスタックに追加するadd関数を追加した

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ fn main() {
 
     stack.push(42);
     stack.push(36);
+    
+    add(&mut stack);
 
     println!("stack: {stack:?}");
+}
+
+fn add(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs + rhs);
 }
